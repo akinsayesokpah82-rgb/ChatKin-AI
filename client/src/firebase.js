4691589ce4977fb8ae6ac1468d471296047c6b94
akinsayesokpah82-rgb <akinsayesokpah82@gmail.com>
2025-11-08 20:21:42 +0000

    Create firebase.js

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// client/src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7cAN-mrE2PvmlQ11zLKAdHBhN7nUFjHw",
+  authDomain: "fir-u-c-students-web.firebaseapp.com",
+  databaseURL: "https://fir-u-c-students-web-default-rtdb.firebaseio.com",
+  projectId: "fir-u-c-students-web",
+  storageBucket: "fir-u-c-students-web.firebasestorage.app",
+  messagingSenderId: "113569186739",
+  appId: "1:113569186739:web:d8daf21059f43a79e841c6"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
